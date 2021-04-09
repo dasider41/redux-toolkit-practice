e098e97b452622b341506a97877cc8ebb5deb94f
dasider41 <dasider41@gmail.com>
2021-04-09 13:52:01 +1200

    Improve store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import todosReducer from "../features/todos/todosSlice";
 
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  todos: todosReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    todos: todosReducer,
-  },
+  reducer: rootReducer,
+  // middleware :[],
 });
